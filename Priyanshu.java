07b551cef58b537c6ed074eb9cfad12ed0ee0670
kaushalkushwah127-cloud <kaushalkushwah127@gmail.com>
2026-01-28 15:48:51 +0530

    first commit

---
+++ Priyanshu.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Priyanshu{
+  public static void main (String [] args ){
+    Scanner s= new Scanner(System.in);
+    System.out.println("kaushal is here for taking \rinput\b:");
+    int a=s.nextInt();
+    System.out.println("Your input :"+a);
+    }
+}
